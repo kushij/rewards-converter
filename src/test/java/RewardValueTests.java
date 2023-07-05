b7f9a39a4007bdb6d85092787589c4de021692e8
kushij <135687855+kushij@users.noreply.github.com>
2023-07-05 01:36:37 -0500

    RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,6 +20,7 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
+        assert false;
         double cashValue = 100;
         int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
         var rewardValue = new RewardValue(cashValue);
@@ -28,6 +29,7 @@ public class RewardValueTests {
 
     @Test
     void convert_from_miles_to_cash() {
+         assert false;
         int milesValue = 10000;
         double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
         var rewardValue = new RewardValue(milesValue);
